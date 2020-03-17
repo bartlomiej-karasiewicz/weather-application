575686502971591bda05515223dc6a5c6ba64448
Bartlomiej Karasiewicz <bartlomiej.karasiewicz7@gmail.com>
2020-03-17 19:22:28 +0100

    Re schedul

--- src/main/java/com/example/synop/config/scheduler/ScheduledTasks.java
+++ src/main/java/com/example/synop/config/scheduler/ScheduledTasks.java
@@ -19,7 +19,7 @@ public class ScheduledTasks {
     private final SynopticFacade synopticFacade;
     private final EmailSender emailSender;
 
-    @Scheduled(cron = "0 */5 * ? * *")
+    @Scheduled(cron = "0 */15 * ? * *")
     public void addAllSynopticParameters(){
         synopticFacade.addMultiSynopticData();
         log.info("Data was provided at "+ DATE_FORMAT.format(new Date()));
