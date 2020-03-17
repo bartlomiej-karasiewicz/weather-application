bcf9d813d304179d0d3aa66f84b9f64a09714afa
Bartlomiej Karasiewicz <bartlomiej.karasiewicz7@gmail.com>
2020-03-17 18:05:49 +0100

    Fix problem with schedulling

--- src/main/java/com/example/synop/config/scheduler/ScheduledTasks.java
+++ src/main/java/com/example/synop/config/scheduler/ScheduledTasks.java
@@ -19,12 +19,12 @@ public class ScheduledTasks {
     private final SynopticFacade synopticFacade;
     private final EmailSender emailSender;
 
-    @Scheduled(cron = "0 0 */4 ? * *")
+    @Scheduled(cron = "0 0 * ? * *")
     public void addAllSynopticParameters(){
         synopticFacade.addMultiSynopticData();
         log.info("Data was provided at "+ DATE_FORMAT.format(new Date()));
     }
-    @Scheduled(cron = "0 45 23 ? * * *")
+    @Scheduled(cron = "0 0 */3 ? * *")
     public void sendMail(){
         emailSender.sendEmailContent();
         log.info("Mail was send at " + DATE_FORMAT.format(new Date()));
